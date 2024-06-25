be828baf0bd01391d8bcb2a57e6875aad0b91681
7onc3k <ngut92@vse.cz>
2024-06-25 23:22:24 +0700

    convert to tailwind

---
+++ src/data/articles.js
@@ -0,0 +1,29 @@
+export const articles = [
+    {
+      title: "Článek 1",
+      slug: "clanek-1",
+      excerpt: "Úvod do článku 1...",
+      date: "2023-05-01",
+      author: "Jan Novák",
+      category: "Webdesign",
+      image: "/images/blog/clanek-1.jpg"
+    },
+    {
+      title: "Článek 2",
+      slug: "clanek-2",
+      excerpt: "Úvod do článku 2...",
+      date: "2023-05-15",
+      author: "Jana Nováková",
+      category: "Vývoj aplikací",
+      image: "/images/blog/clanek-2.jpg"
+    },
+    {
+      title: "Článek 3",
+      slug: "clanek-3",
+      excerpt: "Úvod do článku 3...",
+      date: "2023-06-01",
+      author: "Petr Nový",
+      category: "Digitální marketing",
+      image: "/images/blog/clanek-3.jpg"
+    }
+  ];
