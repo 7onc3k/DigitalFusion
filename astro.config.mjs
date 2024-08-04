bd30e2d6197e65732a7ac23e1ab1e5b38622e3ce
7onc3k <ngut92@vse.cz>
2024-08-04 13:58:56 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+
 import icon from "astro-icon";
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server"
+  output: "server",
+  adapter: node({ mode: 'standalone' })
 });
