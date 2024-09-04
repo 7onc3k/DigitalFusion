4522eb5263363adad7139456ab4b533a5660a0ca
7onc3k <ngut92@vse.cz>
2024-09-04 18:47:04 +0200

    dotazník fullscreen

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
         id: "project-1",
         title: "PDF Překladač s OCR",
         description: "Webová aplikace pro překlad textu z PDF dokumentů s využitím OCR technologie.",
-        image: "public/portfolio/pdf-translator.png",
+        image: "portfolio/pdf-translator.png",
         category: "Webové aplikace",
         technologies: [],
         url: "https://comic-translator.vercel.app/" // Přidáno URL projektu
